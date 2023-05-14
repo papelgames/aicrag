8a343ea62435c0621f7021ebf8db8a72b8ee7c21
Jo <jaltamonte@gmail.com>
2023-05-14 10:52:16 -0300

    rq6fe2

---
+++ app/static/js/mejoras.js
@@ -0,0 +1,27 @@
+function imprSelec(nombre, titulo) {
+    var ficha = document.getElementById(nombre);
+    var ventimp = window.open('', 'popimpr');
+    ventimp.document.write('<html><head><title>'+titulo+'</title>');
+    ventimp.document.write(
+      '<link rel="stylesheet" href="/static/css/bootstrap/bootstrap.min.css">'
+    );
+    ventimp.document.write(
+      '<link rel="stylesheet" href="/static/css/style.css">'
+    );
+    ventimp.document.write('</head><body>');
+    ventimp.document.write(ventimp.document.body.innerHTML);
+    ventimp.document.write('</body></html>');
+    ventimp.document.close();
+    ventimp.print();
+    ventimp.close();
+  }
+
+function enPesos(numero) {
+    const formatoPesos = new Intl.NumberFormat('es-AR', {
+      style: 'currency',
+      currency: 'ARS',
+    });
+  
+    return formatoPesos.format(numero);
+  }
+  
